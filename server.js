bbe34fdbbac75966384730ec3b4c13e56d2fb843
adamki <adajensen@gmail.com>
2016-03-04 03:06:33 -0700

    pollUrls view is now done.

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
-const http    = require('http');
-const express = require('express');
-const app     = express();
+const http      = require('http');
+const express   = require('express');
+const app       = express();
+
+const generateId = require('./lib/generator');
+const Poll       = require('./lib/poll');
+const socketIo  = require('socket.io');
+var bodyParser  = require('body-parser');
+
+var pry = require('pryjs');
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}))
 
 app.set('view engine', 'ejs');
-app.get('/', (req, res) => {
-  res.render('index');
-});
+app.locals.polls = {}
+
 
 var port = process.env.PORT || 3000;
 
@@ -16,8 +24,30 @@ server.listen( port, () => {
   console.log("Listening on PORT: " + port );
 })
 
-const socketIo = require('socket.io');
 const io = socketIo(server);
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.post('/polls', (req, res) => {
+  var id = generateId(10);
+  var adminKey = generateId(3);
+  var pollData = req.body.poll
+  var title = pollData.title
+  var votes = {}
+
+  pollData.responses.forEach(function(response) {
+    votes[response] = 0
+  })
+
+  var newPoll = new Poll(id, adminKey, title, votes)
+
+  app.locals.polls[newPoll.id] = newPoll
+
+  res.render('pollUrls', { poll: newPoll });
+});
+
 
 io.on('connection', function (socket) {
   console.log('A user has connected.', io.engine.clientsCount);
