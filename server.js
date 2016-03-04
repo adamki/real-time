b9e620e65bbdbbc49948018c5f8b8b8ba6e00b0e
adamki <adajensen@gmail.com>
2016-03-03 17:47:32 -0700

    added body parser and other tools. this commit, I added a form to the page and it is routing to a /create path. I also added some kick ass mountains.

--- server.js
+++ server.js
@@ -2,10 +2,19 @@ const http    = require('http');
 const express = require('express');
 const app     = express();
 
+var bodyParser = require('body-parser')
+
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}))
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
+});
+
+app.post('/polls', (req, res) => {
+  console.log(req.body);
+  //res.sendFile(__dirname + '/public/index.html');
 });
 
 var port = process.env.PORT || 3000;
@@ -17,6 +26,7 @@ server.listen( port, () => {
 
 const socketIo = require('socket.io');
 const io = socketIo(server);
+
 
 io.on('connection', function (socket) {
   console.log('A user has connected.', io.engine.clientsCount);
